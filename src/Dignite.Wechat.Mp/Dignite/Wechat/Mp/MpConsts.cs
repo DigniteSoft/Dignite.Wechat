20e144b06d0da1e5d6aa98a6469c077048bfb8ff
杜冠魁 <51013007+duguankui@users.noreply.github.com>
2022-03-14 11:21:44 +0800

    Update MpConsts.cs

--- src/Dignite.Wechat.Mp/Dignite/Wechat/Mp/MpConsts.cs
+++ src/Dignite.Wechat.Mp/Dignite/Wechat/Mp/MpConsts.cs
@@ -4,5 +4,11 @@ namespace Dignite.Wechat.Mp
     public class MpConsts
     {
         public const string HttpClientName = "DigniteWechatMpHttpClient";
+
+
+        /// <summary>
+        /// 微信公众号、小程序联合验证时用的常量
+        /// </summary>
+        public const string UnionAuthenticationScheme = "wechat_union";
     }
 }
